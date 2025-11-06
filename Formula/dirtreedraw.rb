3b08cb6ff88a1b0a01127a11117f2939cb1476f4
Colin Conwell <colinconwell@gmail.com>
2025-11-05 21:29:15 -0500

    Update to v1.1.0

--- Formula/dirtreedraw.rb
+++ Formula/dirtreedraw.rb
@@ -1,11 +1,11 @@
 # Homebrew Formula for DirtreeDraw
-# This is a template - update URLs and SHA256 after publishing to npm
+# Published at: https://github.com/colinconwell/homebrew-dirtreedraw
 
 class Dirtreedraw < Formula
   desc "Visual directory tree builder with IDE-like interface"
   homepage "https://github.com/colinconwell/DirTreeDraw"
-  url "https://registry.npmjs.org/dirtreedraw/-/dirtreedraw-1.0.0.tgz"
-  sha256 "d76041e9329880f1b6ab88ab29925944a236cc66cc264d897ad39b3995216942"
+  url "https://registry.npmjs.org/dirtreedraw/-/dirtreedraw-1.1.0.tgz"
+  sha256 "6f1c46c87b96ff349daf3eea7823e94eba670a6ddf453bcf0cdff595a3c82e0e"
   license "MIT"
 
   depends_on "node"
@@ -21,30 +21,16 @@ class Dirtreedraw < Formula
   end
 end
 
-# Instructions for publishing:
+# Installation:
+#   brew install colinconwell/dirtreedraw/dirtreedraw
 #
-# 1. Publish to npm first:
-#    npm publish
+# Or:
+#   brew tap colinconwell/dirtreedraw
+#   brew install dirtreedraw
 #
-# 2. Get the tarball URL:
-#    https://registry.npmjs.org/dirtreedraw/-/dirtreedraw-VERSION.tgz
-#
-# 3. Calculate SHA256:
-#    curl -sL https://registry.npmjs.org/dirtreedraw/-/dirtreedraw-1.0.0.tgz | shasum -a 256
-#
-# 4. Update this formula with correct URL and SHA256
-#
-# 5. Create a tap repository:
-#    mkdir homebrew-dirtreedraw
-#    cp dirtreedraw.rb homebrew-dirtreedraw/Formula/
-#    cd homebrew-dirtreedraw
-#    git init && git add . && git commit -m "Initial formula"
-#    git remote add origin git@github.com:colinconwell/homebrew-dirtreedraw.git
-#    git push -u origin main
-#
-# 6. Users can then install with:
-#    brew tap colinconwell/dirtreedraw
-#    brew install dirtreedraw
-#
-# Or in one command:
-#    brew install colinconwell/dirtreedraw/dirtreedraw
+# Update for new versions:
+#   1. Update version in URL
+#   2. Calculate new SHA256:
+#      curl -sL https://registry.npmjs.org/dirtreedraw/-/dirtreedraw-VERSION.tgz | shasum -a 256
+#   3. Update sha256 field
+#   4. Commit and push to tap repository
